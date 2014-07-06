1eb10892c6869409f2e21db04925df98c6fcb4b2
HardBite <hardbitec0c0c0@gmail.com>
2014-07-06 16:17:23 +0300

    Adds carousel. Fixes layout

--- app/controllers/user_votes_controller.rb
+++ app/controllers/user_votes_controller.rb
@@ -6,21 +6,7 @@ class UserVotesController < ApplicationController
     @user_vote.user_id = current_user.id
      respond_to do |format|
       if @user_vote.save
-        match_votes = UserVote.all
-        sum = 0
-        count = 0
-        match_votes.each do |v|
-          if v.match_id == params[:match_id].to_i
-            sum = sum + v.vote.to_f
-            count = count +1
-          end
-        end
-        binding.pry
-        average_rating = sum/count
-        match = Match.find_by_id(params[:match_id])
-        match.rating = average_rating
-        match.save
-        
+        recalc_rating(params[:match_id], @user_vote.vote)
         format.html { redirect_to "/matches/#{params[:match_id]}", notice: 'Your opinion is very important for us' }
        else
         format.html { redirect_to :root }
@@ -28,7 +14,23 @@ class UserVotesController < ApplicationController
     end
   end
 
- 
+  def recalc_rating(match_id, vote)
+    match_votes = UserVote.all
+    sum = 0
+    count = 0
+    match_votes.each do |v|
+      if v.match_id == match_id.to_i
+          sum = sum + v.vote.to_f
+          count = count +1
+       end
+     end
+    binding.pry 
+    average_rating = sum/count
+    match = Match.find_by_id(params[:match_id])
+    match.rating = average_rating
+    match.save
+  end
+
   private
      # Never trust parameters from the scary internet, only allow the white list through.
     def user_vote_params
